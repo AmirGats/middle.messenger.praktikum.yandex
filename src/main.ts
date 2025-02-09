0f21f7ed1b357cb4168585fe20b5ab0183611cb0
AmirGats <Amirgatsalov201@gmail.com>
2025-02-09 19:12:12 +0300

    regPartials

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,36 @@
 import "./style.css";
-import Handlebars from 'handlebars';
-import * as Components  from "./components";
-import * as Pages  from "./pages";
+import Handlebars from "handlebars";
+import { registerPartial } from "./utils/regPartials";
+import * as Pages from "./pages";
+
+registerPartial();
 
 const linkPage = {
-  'nav': [ Pages.navPage],
+  'nav': [ Pages.navPage ],
   'login': [ Pages.logPage ],
+  'register': [ Pages.regPage ]
 };
 
-function navigate(page: string){
+function navigate(page: string) {
   //@ts-ignore
-  const [ source, context ] = linkPage[page];
-  const container = document.getElementById('#app')!;
+  const [source, context] = linkPage[page];
+  const container = document.getElementById("app")!;
 
   const templateFunc = Handlebars.compile(source);
   container.innerHTML = templateFunc(context);
 }
 
-Object.entries(Components).forEach(([name, template]) => {
-  Handlebars.registerPartial(name, template);
+document.addEventListener("DOMContentLoaded", () => {
+  navigate("nav");
 });
 
-
-document.addEventListener('DOMContentLoaded', () => {navigate('nav')})
-
-document.addEventListener('click', e => {
+document.addEventListener("click", (e) => {
   //@ts-ignore
-  const page = e.target.getAttribute('page');
+  const page = (e.target as HTMLElement).getAttribute("page");
 
-  if(page){
+  if (page) {
     navigate(page);
     e.preventDefault();
+    e.stopImmediatePropagation();
   }
 });
