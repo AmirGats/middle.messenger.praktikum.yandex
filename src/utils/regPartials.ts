177eb8ea3a493acac0c6c111c4c9f0f45067a9ef
AmirGats <Amirgatsalov201@gmail.com>
2025-02-10 14:02:37 +0300

    added errorPage

--- src/utils/regPartials.ts
+++ src/utils/regPartials.ts
@@ -2,9 +2,12 @@ import Handlebars from "handlebars";
 
 // Импортируем все компоненты
 import * as Components from "../templates/components"
+import * as Template from "../templates/partials/errorContainer"
 
 
 export const registerPartial = () => {
     Handlebars.registerPartial("button", Components.button);
     Handlebars.registerPartial("input", Components.input);
+    Handlebars.registerPartial("link", Components.link);
+    Handlebars.registerPartial("errorContainer", Template.errorContainer);
 }
