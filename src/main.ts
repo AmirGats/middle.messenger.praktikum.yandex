9e17fd647ff04510ff754e06e345ee7811c31ea4
AmirGats <Amirgatsalov201@gmail.com>
2025-02-15 14:05:46 +0300

    added context for img

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,36 @@ import Handlebars from "handlebars";
 import { registerPartial } from "./utils/regPartials";
 import * as Pages from "./pages";
 
+import arrowBack from "./assets/image/arrow_back.svg";
+import arrowForwardLight from "./assets/image/arrow_forward-light.svg";
+import clip from "./assets/image/clip.svg";
+import dotMenu from "./assets/image/dots-menu.svg";
+import profileImg from "./assets/image/profile-img.svg";
 
 registerPartial();
+
 
 const linkPage = {
   'nav': [ Pages.navPage ],
   'login': [ Pages.logPage ],
   'register': [ Pages.regPage ],
-  'profile': [Pages.profile],
-  'profileChange': [Pages.profileChange],
-  'profilePassword': [Pages.profilePassword],
-  'chat': [Pages.chat],
+  'profile': [Pages.profile, {
+    arrowBack: arrowBack,
+    profileImg: profileImg,
+  }],
+  'profileChange': [Pages.profileChange, {
+    arrowBack: arrowBack,
+    profileImg: profileImg,
+  }],
+  'profilePassword': [Pages.profilePassword, {
+    arrowBack: arrowBack,
+    profileImg: profileImg,
+  }],
+  'chat': [Pages.chat, {
+    dotMenu: dotMenu, 
+    clipMenu: clip, 
+    arrowLight: arrowForwardLight, 
+  }],
   'error404': [Pages.error404],
   'error500': [Pages.error500],
 };
@@ -37,7 +56,6 @@ function navigate(page: string) {
       phone: "+7 (999) 123-45-67"
     };
   }
-
 
   const templateFunc = Handlebars.compile(source);
   container.innerHTML = templateFunc(profileData);
