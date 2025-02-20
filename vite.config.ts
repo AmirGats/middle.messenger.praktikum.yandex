d51dd86ccb66403b4c69e8e7951ecffd925663ad
AmirGats <Amirgatsalov201@gmail.com>
2025-02-21 00:09:43 +0300

    added vite-plugin-checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import {defineConfig} from 'vite';
+import checker from "vite-plugin-checker";
 
 export default defineConfig({
     root: '.',
     build: {
         outDir: 'dist'
     },
-})
+    plugins: [
+        checker({
+            typescript: true,
+        }),
+    ],
+});
